9eda3109558c9e7be2d6b7744369df2c5301068e
sneha9989 <134386383+sneha9989@users.noreply.github.com>
2024-01-26 12:10:22 +0530

    Add files via upload

---
+++ ksp_task4/first.js
@@ -0,0 +1,47 @@
+console.log("welcome to javascript");
+//operators
+let val1=1;
+let val2=3;
+console.log(val1+val2);
+console.log(val1-val2);
+console.log(val1*val2);
+console.log(val1/val2);
+console.log(val1%val2);
+console.log(val1**val1);
+console.log(val1++);
+console.log(val1--);
+console.log(++val2);
+console.log(--val2);
+//comparision operators <=,>=,==,!=,<,>,===,!==,?
+console.log(val1<=val2);
+console.log(val1>=val2);
+console.log(val1==val2);
+console.log(val1!=val2);
+console.log(val1<val2);
+console.log(val1>val2);
+//Assignment operators
+console.log(val1=val2);
+console.log(val1+=val2);
+console.log(val1-=val2);
+console.log(val1*=val2);
+console.log(val1/=val2);
+console.log(val1%=val2);
+console.log(val1**=val2);
+//conditional statements
+if(val1<val2) {
+    console.log("correct");
+}
+else {
+    console.log("statement is not correct");
+}
+while(val1<val2)
+{
+    console.log("expression");
+    val1++;
+}
+
+
+
+
+
+
